57451793318a7cbb7f746b383da8d266d3ac5b27
Matthew McCabe <matthew.james.mccabe@outlook.com>
2022-11-07 23:11:33 -0600

    created more calculations and interesting output

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ with open('WeatherDataWindows.csv', mode='r') as file:
     datafile = csv.DictReader(file)
     max_temps = []
     min_temps = []
+    avg_temps = []
     precip = []
+    months = []
+    hum = []
+    bad_day = []
+    ovr_95 = []
+    undr_50 = []
+
 
     # for loop to loop through each line in weather data file
     for lines in datafile:
@@ -15,6 +22,12 @@ with open('WeatherDataWindows.csv', mode='r') as file:
         max_temps.append(int(lines['Temp High']))
         min_temps.append(int(lines['Temp Low']))
         precip.append(float(lines['Precipitation (in.)']))
+        months.append(lines['Date'])
+        hum.append(int(lines['Humidity Avg']))
+
+    for i in hum:
+        if i > 95:
+            bad_day.append(i)
 
     # sorting temp lists to get highest and lowest temp on 3 yr period and assigning
     max_temps.sort()
@@ -22,10 +35,25 @@ with open('WeatherDataWindows.csv', mode='r') as file:
     sent_max = max_temps[-1]
     sent_min = min_temps[0]
 
+    for i in max_temps:
+        if i > 95:
+            ovr_95.append(i)
+    for i in min_temps:
+        if i < 50:
+            undr_50.append(i)
+
     # precipitation calc.
     avg_precip = sum(precip) / len(precip)
+    bad_days = len(bad_day)
+    sum_ovr_95 = len(ovr_95)
+    sum_undr_50 = len(undr_50)
 
     # formatted p statements for readability
-    print(f'The max. and min. temp over the 3 year period was {sent_max}, and {sent_min} respectively')
-    print(f'The average daily precipitation across three years was {avg_precip:.2f} in.')
+    print(f'\n\tThe max. and min. temp over the 3 year period was {sent_max} deg. f, and {sent_min} deg. f respectively')
+    print(f'\tThe average daily precipitation across three years was {avg_precip:.2f} in.')
+    # interesting analysis statements
+    print('\n\t\t\t\t-----------------Hmmmm... Interesting-----------------')
+    print(f'\tOver the 3 year period, there were {bad_days} days where the avg humidity was above 95')
+    print(f'\tFurthermore, the amount of days where the temp was below 50 is {sum_undr_50}, hope you brought a parka!')
+    print(f'\tFinally, the amount of days where the temp was above 95 is {sum_ovr_95}, WOW toasty!')
 
