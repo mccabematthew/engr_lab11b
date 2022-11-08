7cee658011e552841d523b2f0dcacce856fc4518
Matthew McCabe <matthew.james.mccabe@outlook.com>
2022-11-07 22:38:50 -0600

    operated on data and stored in lists for further use. output formatted data as requested (by Professor Ball)

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import csv
 
+# open csv as generic var.
 with open('WeatherDataWindows.csv', mode='r') as file:
 
-    weather_file = csv.reader(file)
-    weather = file.readlines()
-    next_line = file.readline()
+    # reading csv data as dicts and creating lists for later purpose
+    datafile = csv.DictReader(file)
+    max_temps = []
+    min_temps = []
+    precip = []
 
-    #print(extreme_high)
+    # for loop to loop through each line in weather data file
+    for lines in datafile:
+        # transforming weather data to approp. data type, and adding to list
+        max_temps.append(int(lines['Temp High']))
+        min_temps.append(int(lines['Temp Low']))
+        precip.append(float(lines['Precipitation (in.)']))
 
-    while next_line != '':
-        file.readline()
-        weather.split()
-        print(next_line, end='')
+    # sorting temp lists to get highest and lowest temp on 3 yr period and assigning
+    max_temps.sort()
+    min_temps.sort()
+    sent_max = max_temps[-1]
+    sent_min = min_temps[0]
 
-#def extreme_temps():
-#   if
+    # precipitation calc.
+    avg_precip = sum(precip) / len(precip)
+
+    # formatted p statements for readability
+    print(f'The max. and min. temp over the 3 year period was {sent_max}, and {sent_min} respectively')
+    print(f'The average daily precipitation across three years was {avg_precip:.2f} in.')
+
